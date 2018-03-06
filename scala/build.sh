12a776fa2808d59fe83d443562435c3334ada7d4
Daniel P. Brice <danielbrice@gmail.com>
2018-03-06 00:54:33 -0800

    Update build.sh

--- scala/build.sh
+++ scala/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# assumes you have java and scala installed and on your path
+
+# once compiled, run `Foo.class` by invoking `scala Foo`
+
 scalac OrdinaryPartiality.scala  && \
 scalac LibMaybe.scala            && \
 scalac MonadicPartiality.scala   && \
